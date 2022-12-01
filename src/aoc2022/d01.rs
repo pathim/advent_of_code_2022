7ca46bf18c6a2261166c2d291d43042916071c03
Patrick Himmelmann <himmelmannp@gmail.com>
2022-12-01 19:10:01 +0100

    Day 1

--- src/aoc2022/d01.rs
+++ src/aoc2022/d01.rs
@@ -1,3 +1,25 @@
-pub fn f(input: std::fs::File) -> crate::AocResult {
-    ("dummy".into(), None)
+use std::io::BufRead;
+
+pub fn f(file: std::fs::File) -> crate::AocResult {
+    let input = std::io::BufReader::new(file);
+    let mut cal = Vec::new();
+    let mut current = 0;
+    for line in input.lines() {
+        let line = line.expect("Read error");
+        let line = line.trim();
+        if line.is_empty() {
+            cal.push(current);
+            current = 0;
+            continue;
+        }
+        let val: u32 = line.parse().expect("Not an integer");
+        current += val;
+    }
+    cal.push(current);
+    cal.sort();
+
+    let res1 = cal.pop().expect("No values available");
+    let res2 = res1 + cal.pop().unwrap() + cal.pop().unwrap();
+
+    (format!("{}", res1), Some(format!("{}", res2)))
 }
