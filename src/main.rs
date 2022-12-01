990fd75550204322f9cc9386b014ed11b877f549
Patrick Himmelmann <himmelmannp@gmail.com>
2022-12-01 19:14:26 +0100

    command line argument to run all or only last day

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod aoc2022;
 mod input;
 
+use clap::Parser;
+
 type AocResult = (String, Option<String>);
 type AocFun = fn(std::fs::File) -> AocResult;
 
@@ -11,9 +13,9 @@ fn wrap_func(func: &AocFun, input: Result<std::fs::File, input::Error>) -> AocRe
     }
 }
 
-fn output_result<T:FnOnce()->AocResult>(res: T) {
-    let time_begin=std::time::SystemTime::now();
-    let res=res();
+fn output_result<T: FnOnce() -> AocResult>(res: T) {
+    let time_begin = std::time::SystemTime::now();
+    let res = res();
     println!("--- Part 1 ---");
     println!("{}", res.0);
     if let Some(res) = res.1 {
@@ -21,44 +23,61 @@ fn output_result<T:FnOnce()->AocResult>(res: T) {
         println!("{}", res);
     }
     println!("--- Time ---");
-    println!("{}s",time_begin.elapsed().unwrap_or_default().as_secs_f64());
+    println!(
+        "{}s",
+        time_begin.elapsed().unwrap_or_default().as_secs_f64()
+    );
 }
 
-fn output_results(year: i32, funcs: & [AocFun]) {
+fn output_all_results(year: i32, funcs: &[AocFun]) {
     let results = get_results(year, funcs);
     let start = std::time::SystemTime::now();
     for (i, res) in results {
         println!("==== Day {} ====", i + 1);
         output_result(res);
-        println!("");
+        println!();
     }
     println!(
         "Total time: {}s",
         start.elapsed().unwrap_or_default().as_secs_f64()
     );
 }
-fn get_results<'a>(year:i32,funcs:&'a [AocFun]) ->impl Iterator<Item=(usize,Box<impl FnOnce()->AocResult+'a>)>{
+fn get_results(
+    year: i32,
+    funcs: &[AocFun],
+) -> impl Iterator<Item = (usize, Box<impl FnOnce() -> AocResult + '_>)> {
     let input = input::get_all_inputs(year);
     let results = funcs
-        .into_iter()
+        .iter()
         .zip(input)
         .map(|(func, input)| Box::new(move || wrap_func(func, input)))
         .enumerate();
-        results
+    results
 }
 
-fn output_last_results(year: i32, funcs: & [AocFun]) {
+fn output_last_results(year: i32, funcs: &[AocFun]) {
     let results = get_results(year, funcs);
-    for (i, res) in results.last() {
+    if let Some((i, res)) = results.last() {
         println!("==== Day {} ====", i + 1);
         output_result(res);
-        println!("");
+        println!();
     }
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // calculate all days
+    #[arg(short, long)]
+    pub all: bool,
+}
+
 fn main() {
-    let funcs2021 = aoc2021::get_funcs();
+    let args = Args::parse();
     let funcs2022 = aoc2022::get_funcs();
-    output_last_results(2021, &funcs2021);
-    output_results(2022, &funcs2022);
+    if args.all {
+        output_all_results(2022, &funcs2022);
+    } else {
+        output_last_results(2022, &funcs2022);
+    }
 }
