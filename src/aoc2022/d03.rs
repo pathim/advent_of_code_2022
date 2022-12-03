59e49318322a8514121ff11a4b28b835b2c3f5ca
Patrick Himmelmann <himmelmannp@gmail.com>
2022-12-03 16:41:09 +0100

    Optimize day 3 runtime

--- src/aoc2022/d03.rs
+++ src/aoc2022/d03.rs
@@ -1,38 +1,40 @@
-use std::collections::HashSet;
 use std::io::BufRead;
 
-fn get_prio(c: &char) -> u32 {
-    *c as u32
+fn get_prio(c: &char) -> u64 {
+    *c as u64
         - if c.is_uppercase() {
-            'A' as u32 - 27
+            'A' as u64 - 27
         } else {
-            'a' as u32 - 1
+            'a' as u64 - 1
         }
+}
+
+fn str_to_u64(s: &str) -> u64 {
+    s.chars()
+        .map(|c| get_prio(&c))
+        .map(|v| 1 << v)
+        .fold(0, std::ops::BitOr::bitor)
 }
 
 pub fn f(file: std::fs::File) -> crate::AocResult {
     let input = std::io::BufReader::new(file);
     let mut res1 = 0;
     let mut res2 = 0;
-    let mut common_badge = HashSet::new();
+    let mut common_badge = u64::MAX;
     for (i, line) in input.lines().enumerate() {
         let line = line.unwrap();
         let line = line.trim();
         let l = line.len() / 2;
-        let all = line.chars().collect::<HashSet<_>>();
-        common_badge = if common_badge.is_empty() {
-            all
-        } else {
-            common_badge.intersection(&all).copied().collect()
-        };
+        let all = str_to_u64(line);
+        common_badge &= all;
         if i % 3 == 2 {
-            res2 += get_prio(common_badge.iter().next().unwrap());
-            common_badge.clear();
+            res2 += common_badge.trailing_zeros();
+            common_badge = u64::MAX;
         }
-        let s1 = line[..l].chars().collect::<HashSet<_>>();
-        let s2 = line[l..].chars().collect::<HashSet<_>>();
-        let common = s1.intersection(&s2).next().unwrap();
-        res1 += get_prio(common);
+        let s1 = str_to_u64(&line[..l]);
+        let s2 = str_to_u64(&line[l..]);
+        let common = s1 & s2;
+        res1 += common.trailing_zeros();
     }
     (res1, res2).into()
 }
