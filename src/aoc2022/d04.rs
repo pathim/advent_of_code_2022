0d3475bbe071cadf26590745a4471a417593ec40
Patrick Himmelmann <himmelmannp@gmail.com>
2022-12-04 17:38:33 +0100

    use u16 on day 4 to get native len-method on ranges

--- src/aoc2022/d04.rs
+++ src/aoc2022/d04.rs
@@ -1,21 +1,17 @@
 use std::{io::BufRead, ops::RangeInclusive};
 
-fn range_from_str(s: &str) -> RangeInclusive<usize> {
+fn range_from_str(s: &str) -> RangeInclusive<u16> {
     let (start, end) = s.trim().split_once('-').unwrap();
-    let start: usize = start.parse().unwrap();
-    let end: usize = end.parse().unwrap();
+    let start: u16 = start.parse().unwrap();
+    let end: u16 = end.parse().unwrap();
     start..=end
 }
 
-fn ranges_from_str(s: &str) -> (RangeInclusive<usize>, RangeInclusive<usize>) {
+fn ranges_from_str(s: &str) -> (RangeInclusive<u16>, RangeInclusive<u16>) {
     let (a, b) = s.split_once(',').unwrap();
     let r1 = range_from_str(a);
     let r2 = range_from_str(b);
     (r1, r2)
-}
-
-fn range_len(r: &RangeInclusive<usize>) -> usize {
-    r.end() - r.start() + 1
 }
 
 pub fn f(file: std::fs::File) -> crate::AocResult {
@@ -24,7 +20,7 @@ pub fn f(file: std::fs::File) -> crate::AocResult {
     let mut res2 = 0;
     for l in input.lines() {
         let (r1, r2) = ranges_from_str(&l.unwrap());
-        let (long, short) = if range_len(&r1) > range_len(&r2) {
+        let (long, short) = if r1.len() > r2.len() {
             (r1, r2)
         } else {
             (r2, r1)
