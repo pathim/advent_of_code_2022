9dfd513fa4b6c8bd5ecdf7be185f1980446aeb7a
Patrick Himmelmann <patrick.himmelmann@kit.edu>
2022-12-28 14:36:33 +0100

    Day 21

--- src/aoc2022/d21.rs
+++ src/aoc2022/d21.rs
@@ -1,6 +1,6 @@
-use std::{io::BufRead, str::FromStr};
+use std::{fmt::Display, io::BufRead, str::FromStr};
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Op {
     Add,
     Sub,
@@ -29,6 +29,82 @@ impl Op {
             Op::Sub => lhs - rhs,
             Op::Mul => lhs * rhs,
             Op::Div => lhs / rhs,
+        }
+    }
+}
+
+impl Display for Op {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Op::Add => write!(f, "+"),
+            Op::Sub => write!(f, "-"),
+            Op::Mul => write!(f, "*"),
+            Op::Div => write!(f, "/"),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum Val {
+    Var,
+    Num(i64),
+    Op(Op, Box<Val>, Box<Val>),
+}
+
+impl Display for Val {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Var => write!(f, "X"),
+            Self::Num(v) => write!(f, "{}", v),
+            Self::Op(o, v1, v2) => write!(f, "({} {} {})", v1, o, v2),
+        }
+    }
+}
+
+impl Val {
+    fn simplify(self) -> Self {
+        match self {
+            Self::Op(o, v1, v2) => {
+                let v1 = v1.simplify();
+                let v2 = v2.simplify();
+                match (v1, v2) {
+                    (Self::Num(v1), Self::Num(v2)) => Self::Num(o.eval(v1, v2)),
+                    (v1, v2) => Self::Op(o, Box::new(v1), Box::new(v2)),
+                }
+            }
+            v => v,
+        }
+    }
+    fn solve(self, other: Self) -> (Self, Self) {
+        let res = match self {
+            Self::Op(o, v1, v2) => match o {
+                Op::Add => {
+                    if v1.complexity() >= v2.complexity() {
+                        (*v1, Val::Op(Op::Sub, Box::new(other), v2))
+                    } else {
+                        (*v2, Val::Op(Op::Sub, Box::new(other), v1))
+                    }
+                }
+                Op::Sub => (*v1, Val::Op(Op::Add, Box::new(other), v2)),
+                Op::Mul => {
+                    if v1.complexity() >= v2.complexity() {
+                        (*v1, Val::Op(Op::Div, Box::new(other), v2))
+                    } else {
+                        (*v2, Val::Op(Op::Div, Box::new(other), v1))
+                    }
+                }
+                Op::Div => (*v1, Val::Op(Op::Mul, Box::new(other), v2)),
+            },
+            v => (v, other),
+        };
+        (res.0.simplify(), res.1.simplify())
+    }
+
+    fn complexity(&self) -> usize {
+        match self {
+            Self::Var => 0,
+            Self::Num(_) => 0,
+            Self::Op(_, v1, v2) => 1 + v1.complexity().max(v2.complexity()),
         }
     }
 }
@@ -79,6 +155,23 @@ impl Monkey {
             )
         }
     }
+    fn to_val(&self, monkeys: &std::collections::HashMap<String, Monkey>) -> Val {
+        if let Some(v) = self.value {
+            Val::Num(v)
+        } else {
+            let rhs = Box::new(if self.rhs == "humn" {
+                Val::Var
+            } else {
+                monkeys[&self.rhs].to_val(monkeys)
+            });
+            let lhs = Box::new(if self.lhs == "humn" {
+                Val::Var
+            } else {
+                monkeys[&self.lhs].to_val(monkeys)
+            });
+            Val::Op(self.op, lhs, rhs)
+        }
+    }
 }
 
 pub fn f(file: std::fs::File) -> crate::AocResult {
@@ -91,5 +184,21 @@ pub fn f(file: std::fs::File) -> crate::AocResult {
         monkeys.insert(name.to_string(), monkey);
     }
     let res1 = monkeys["root"].eval(&monkeys);
-    res1.into()
+    let mut lhs = monkeys[&monkeys["root"].lhs].to_val(&monkeys).simplify();
+    let mut rhs = monkeys[&monkeys["root"].rhs].to_val(&monkeys).simplify();
+    let (lhs, rhs) = loop {
+        let (nlhs, nrhs) = lhs.clone().solve(rhs.clone());
+        let (nrhs, nlhs) = nrhs.solve(nlhs);
+        if nlhs == lhs {
+            break (lhs, rhs);
+        }
+        lhs = nlhs;
+        rhs = nrhs;
+    };
+    let res2 = match (lhs, rhs) {
+        (Val::Var, Val::Num(v)) => v,
+        (Val::Num(v), Val::Var) => v,
+        _ => panic!("Could not solve rhs"),
+    };
+    (res1, res2).into()
 }
